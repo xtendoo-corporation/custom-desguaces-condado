e8bd536bbd3f8b2504af9f8a101762a148e31762
Gokac9 <theabraham9@gmail.com>
2025-08-22 13:36:54 +0200

    test obtain pieces

--- metasync_api_connector/wizards/recover_changes_stock_company_metasync_wizard.py
+++ metasync_api_connector/wizards/recover_changes_stock_company_metasync_wizard.py
@@ -306,7 +306,7 @@ class RecoverChangesStockCompanyMetasyncWizard(models.TransientModel):
                 'modification_date': formatted_date,
                 'cod_almacen': pieza.get('codAlmacen', ''),
                 'website_published': True,
-                'product_vehicle_id': id_vehiculo,  # Relación con el vehículo
+                'vehicle_id': id_vehiculo,  # Relación con el vehículo
             }
 
             if product:
